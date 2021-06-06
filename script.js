c5519af8da3d5e31e1fdf44972e6f8d2916c89fd
NSkeva <nikola.skeva1@gmail.com>
2021-06-06 18:20:47 +0200

    DA

---
+++ script.js
@@ -0,0 +1,68 @@
+(function(){
+  emailjs.init("user_2DhFWfxbcSV1gEGdmEZJM");
+})();
+
+var vdolazak = document.getElementById("dolazak").value;
+var vodlazak=document.getElementById("odlazak").value;
+var vodrasli=document.getElementById("odrasli").value;
+var vdjeca=document.getElementById("djeca").value;
+var vmail=document.getElementById("mail").value;
+
+function sendEmail(){
+console.log("sending...");
+
+  const templateParams = {
+    dolazak: document.getElementById("dolazak").value,
+    odlazak: document.getElementById("odlazak").value,
+    odrasli: document.getElementById("odrasli").value,
+    djeca: document.getElementById("djeca").value,
+    mail: document.getElementById("mail").value
+  };
+
+  emailjs.send('service_fhic7rk','apartmani_ID', templateParams, 'user_2DhFWfxbcSV1gEGdmEZJM')
+    .then((response) => {
+      console.log('SUCCESS!', response.status, response.text);
+    }, (err) => {
+      console.log('FAILED...', err);
+    });
+
+    setTimeout(() => { console.log("sent!!"); }, 2000)
+}
+
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+  }
+   
+  function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+  }
+  
+  // Slideshow Apartment Images
+  var slideIndex = 1;
+  showDivs(slideIndex);
+  
+  function plusDivs(n) {
+    showDivs(slideIndex += n);
+  }
+  
+  function currentDiv(n) {
+    showDivs(slideIndex = n);
+  }
+  
+  function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+    }
+    x[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " w3-opacity-off";
+  }
